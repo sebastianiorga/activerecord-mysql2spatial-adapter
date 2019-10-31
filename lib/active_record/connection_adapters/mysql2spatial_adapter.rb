b12a4537d040730a5d190a7cb63bc35d9fc8fc07
Kazuo Matsunaga <kazuomatz@icloud.com>
2019-10-31 14:55:04 +0900

    rails 6 support

--- lib/active_record/connection_adapters/mysql2spatial_adapter.rb
+++ lib/active_record/connection_adapters/mysql2spatial_adapter.rb
@@ -41,6 +41,39 @@ require 'active_record/connection_adapters/mysql2_adapter'
 # See https://github.com/rgeo/rgeo-activerecord/issues/23
 module RGeo
   module ActiveRecord
+
+    DEFAULT_SPATIAL_COLUMN_CONSTRUCTORS = {
+        geometry_collection: {}.freeze,
+        geometry:            {}.freeze,
+        line_string:         {}.freeze,
+        multi_line_string:   {}.freeze,
+        multi_point:         {}.freeze,
+        multi_polygon:       {}.freeze,
+        point:               {}.freeze,
+        polygon:             {}.freeze,
+        spatial:             { type: "geometry" }.freeze,
+    }.freeze
+
+
+    # Index definition struct with a spatial flag field.
+
+    SpatialIndexDefinition = Struct.new(:table, :name, :unique, :columns, :lengths, :orders, :where, :spatial)
+
+    # Returns a feature type module given a string type.
+
+    def self.geometric_type_from_name(name)
+      case name.to_s
+      when /^geometrycollection/i then Feature::GeometryCollection
+      when /^geometry/i then Feature::Geometry
+      when /^linestring/i then Feature::LineString
+      when /^multilinestring/i then Feature::MultiLineString
+      when /^multipoint/i then Feature::MultiPoint
+      when /^multipolygon/i then Feature::MultiPolygon
+      when /^point/i then Feature::Point
+      when /^polygon/i then Feature::Polygon
+      end
+    end
+
     class SpatialIndexDefinition
       def using
       end
@@ -64,7 +97,6 @@ module ActiveRecord
   # this class.
 
   class Base
-
 
     # Create a mysql2spatial connection adapter.
 
